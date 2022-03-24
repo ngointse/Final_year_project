c9857501c7f5272c7dcf947b1074b991738060e3
ngointse <ngointse@gmail.com>
2022-03-24 21:10:52 +0800

    new

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  gvbhjgyuj
+
+  <hello-world /></div>
+</template>
+
+<script>
+  // import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'HomePage',
+
+    components: {
+      // HelloWorld,
+    },
+  }
+</script>
